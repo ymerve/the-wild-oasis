0d3ac94db1977d1729752e6430e808b59e274baf
merve <merveyldrm9497@gmail.com>
2024-05-31 18:15:01 +0300

    Created project.

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Booking from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+import Account from "./pages/Account";
+
+function App() {
+    return (
+        <>
+            <GlobalStyles />
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<AppLayout />}>
+                        <Route
+                            index
+                            element={<Navigate replace to="dashboard" />}
+                        />
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="booking" element={<Booking />} />
+                        <Route path="cabins" element={<Cabins />} />
+                        <Route path="users" element={<Users />} />
+                        <Route path="settings" element={<Settings />} />
+                        <Route path="account" element={<Account />} />
+                    </Route>
+                    <Route path="login" element={<Login />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
