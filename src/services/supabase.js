a64243a1b81b4f7599f429c1db0c32f879cba748
merve <merveyldrm9497@gmail.com>
2024-06-04 16:03:02 +0300

    Supabase: Connecting supabase with react.
    Added cabins api with use supabase.

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://qolagebtjdrkfrnahdan.supabase.co';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvbGFnZWJ0amRya2ZybmFoZGFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTczNTMzMTQsImV4cCI6MjAzMjkyOTMxNH0.LpemHSfWFqAeoLuJw_HOHT-Zvc-yKkU4RSnzrl39VKs";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
