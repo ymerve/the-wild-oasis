1bc7331c5fb485b36a5c3b4d739115937b46a99c
merve <merveyldrm9497@gmail.com>
2024-06-12 15:25:31 +0300

    Advanced React Patterns: Building a modal window
    a react portal.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,14 +1,9 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
-import { useState } from "react";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-    const [showForm, setShowForm] = useState(false);
-
     return (
         <>
             <Row type="horizontal">
@@ -18,13 +13,8 @@ function Cabins() {
 
             <Row>
                 <CabinTable />
+                <AddCabin />
             </Row>
-
-            <Button onClick={() => setShowForm((show) => !show)}>
-                Add new cabin
-            </Button>
-
-            {showForm && <CreateCabinForm />}
         </>
     );
 }
