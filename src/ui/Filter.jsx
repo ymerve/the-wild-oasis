15c2e284975ddea607d29dbd1543c87c69574d94
merve <merveyldrm9497@gmail.com>
2024-06-27 22:20:51 +0300

    Add client-side filtering for cabins table.

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,35 +1,65 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const StyledFilter = styled.div`
-  border: 1px solid var(--color-grey-100);
-  background-color: var(--color-grey-0);
-  box-shadow: var(--shadow-sm);
-  border-radius: var(--border-radius-sm);
-  padding: 0.4rem;
-  display: flex;
-  gap: 0.4rem;
+    border: 1px solid var(--color-grey-100);
+    background-color: var(--color-grey-0);
+    box-shadow: var(--shadow-sm);
+    border-radius: var(--border-radius-sm);
+    padding: 0.4rem;
+    display: flex;
+    gap: 0.4rem;
 `;
 
 const FilterButton = styled.button`
-  background-color: var(--color-grey-0);
-  border: none;
+    background-color: var(--color-grey-0);
+    border: none;
 
-  ${(props) =>
-    props.active &&
-    css`
-      background-color: var(--color-brand-600);
-      color: var(--color-brand-50);
-    `}
+    ${(props) =>
+        props.active &&
+        css`
+            background-color: var(--color-brand-600);
+            color: var(--color-brand-50);
+        `}
 
-  border-radius: var(--border-radius-sm);
-  font-weight: 500;
-  font-size: 1.4rem;
-  /* To give the same height as select */
-  padding: 0.44rem 0.8rem;
-  transition: all 0.3s;
+    border-radius: var(--border-radius-sm);
+    font-weight: 500;
+    font-size: 1.4rem;
+    /* To give the same height as select */
+    padding: 0.44rem 0.8rem;
+    transition: all 0.3s;
 
-  &:hover:not(:disabled) {
-    background-color: var(--color-brand-600);
-    color: var(--color-brand-50);
-  }
+    &:hover:not(:disabled) {
+        background-color: var(--color-brand-600);
+        color: var(--color-brand-50);
+    }
 `;
+
+function Filter({ filterField, options }) {
+    const [searchParam, setSearchParam] = useSearchParams();
+
+    const currentFilter = searchParam.get(filterField) || options.at(0).value;
+    console.log(currentFilter);
+
+    function handleClick(value) {
+        searchParam.set(filterField, value);
+        setSearchParam(searchParam);
+    }
+    return (
+        <StyledFilter>
+            {options.map((option) => (
+                <FilterButton
+                    key={option.value}
+                    onClick={() => {
+                        handleClick(option.value);
+                    }}
+                    active={option.value === currentFilter ? 1 : 0}
+                >
+                    {option.label}
+                </FilterButton>
+            ))}
+        </StyledFilter>
+    );
+}
+
+export default Filter;
