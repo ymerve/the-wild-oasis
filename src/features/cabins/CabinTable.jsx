789654f710c757db225ced828ce4152eee38e6f3
merve <merveyldrm9497@gmail.com>
2024-06-28 17:36:43 +0300

    Building the bookings table.

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,12 +4,15 @@ import useCabin from "./useCabin";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
     const { isLoading, cabins } = useCabin();
     const [searchParam] = useSearchParams();
 
     if (isLoading) return <Spinner />;
+
+    if (!cabins.length) return <Empty resourceName="cabins" />;
 
     // 1-) FILTER
     const filterValue = searchParam.get("discount") || "all";
