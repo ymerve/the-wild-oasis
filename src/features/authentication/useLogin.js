5fa7bfd4d0faecf816d943bafeb578ae6cfd0d45
merve <merveyldrm9497@gmail.com>
2024-07-01 17:54:04 +0300

    Authorization: Protecting routes.

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuth";
 import { toast } from "react-hot-toast";
+import supabase from "../../services/supabase";
 
 export function useLogin() {
+    const queryClient = useQueryClient();
     const navigate = useNavigate();
 
     const { mutate: login, isLoading } = useMutation({
         mutationFn: ({ email, password }) => loginApi({ email, password }),
         onSuccess: (user) => {
-            console.log(user);
+            queryClient.setQueriesData(["user"], user);
             navigate("/dashboard");
         },
         onError: (err) => {
@@ -19,4 +21,15 @@ export function useLogin() {
     });
 
     return { login, isLoading };
+}
+
+export async function getCurrentUser() {
+    const { data: session } = await supabase.auth.getSession();
+    if (!session.session) return null;
+
+    const { data, error } = await supabase.auth.getUser();
+
+    if (error) throw new Error(error.message);
+
+    return data?.user;
 }
