5d2ae55cedc862fdd5d6b1c600273c997a18e450
merve <merveyldrm9497@gmail.com>
2024-07-02 22:55:38 +0300

    Building the dashboard layout.

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto 34rem auto;
+    gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+    return (
+        <StyledDashboardLayout>
+            <div>Statustics</div>
+            <div>Today's activity</div>
+            <div>Chart stay duration</div>
+            <div>Chart sales</div>
+        </StyledDashboardLayout>
+    );
+}
+
+export default DashboardLayout;
