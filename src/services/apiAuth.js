b41d147e010304a649f0c896a1b31858b2b5b6f4
merve <merveyldrm9497@gmail.com>
2024-07-02 16:30:52 +0300

    Fixing update react query cashe.

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -73,7 +73,7 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
     if (StorageError) throw new Error(StorageError.message);
 
     // 3. Update avatar in the user
-    const { data: updatedUser, error: error2 } = supabase.auth.updateUser({
+    const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
         data: {
             avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
         }
