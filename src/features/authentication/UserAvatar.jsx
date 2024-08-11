c1ce3f968a4446b470cb67e601db3e3cb07231f3
merve <merveyldrm9497@gmail.com>
2024-08-11 15:32:16 +0300

    Remove log message.

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -25,7 +25,6 @@ const Avatar = styled.img`
 function UserAvatar() {
     const { user } = useUser();
     const { fullName, avatar } = user.user_metadata;
-    console.log(fullName, "oo");
 
     return (
         <StyledUserAvatar>
